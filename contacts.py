54945d448e4a41f7a06cf7d0dc7bb5ebc721921e
joao_cruz <joaovcruz50@gmail.com>
2025-01-02 19:53:51 -0300

    up

--- contacts.py
+++ contacts.py
@@ -1,14 +1,18 @@
 contatos = {
-    "joao": {
+    0: {
         "name": "Joao",
         "number": "5535997478472"
     },
-    "gustavo": {
+    1: {
         "name": "Gustavo",
         "number": "553597184807"
     },
-    "yan": {
+    2: {
         "name": "Yan",
+        "number": "5535999218509"
+    }
+    3: {
+        "name": "Jabu",
         "number": "5535999218509"
     }
 }
